ab1a6b8a2e2c65b53d4f8b320bea04fb95607d2c
Tushar Saurabh <tusharacc@gmail.com>
2021-04-04 23:06:07 -0400

    CPP Sel Sort added performance.csv logic

--- selectionsort/cpp/main.cpp
+++ selectionsort/cpp/main.cpp
@@ -1,24 +1,56 @@
 #include <iostream>
+#include <selectionsort.h>
+#include <stdlib.h>
+#include <ctime> 
+#include <fstream>
+
 using namespace std;
 
-int main (int argc, char *argv[])
-{
-	int foo [5] = { 16, 2, 77, 77, 71 };
+#define MAX_CASES 100;
+#define MAX_ITEMS 100000;
+#define MAX  100000;
+
+void sortArray(int *arr, int length){
 	int temp;
-	for (int i = 0; i < 5; i++){
+
+	for (int i = 0; i < length; i++){
+		cout << "Processing ith item " << i << endl;
 		int index = i;
-		for (int j = i + 1; j < 5; j++){
-			if (foo[i] > foo[j]){
+		for (int j = i + 1; j < length; j++){
+			if (arr[i] > arr[j]){
 				index = j;
 			}
 		}
-		temp = foo[i];
-		foo[i] = foo[index];
-		foo[index] = temp;
+		temp = arr[i];
+		arr[i] = arr[index];
+		arr[index] = temp;
 	}
+}
 
-	for (int i = 0; i < 5; i++){
-		cout << foo[i] << endl;
+int main (int argc, char *argv[])
+{
+	int caseCount = 1;
+	
+	ofstream myfile;
+    myfile.open ("../performance.csv");
+
+    myfile << "program_type,number_of_items,start_time,end_time,time_taken,result\n";
+
+	while (caseCount > 0)
+	{
+		int itemCount = rand() % MAX_ITEMS + 1; 
+		int arr[itemCount] ={0};
+		for (int i = 0; i < itemCount; i++)
+		{	
+			arr[i] = rand() % MAX + 1;
+		}
+		cout << "The itemCount is " << itemCount << endl;
+		time_t start = time(0);
+		sortArray(arr,itemCount);
+		time_t end = time(0);
+		myfile << "SELSORT.CPP," << itemCount << "," << start << "," << end << "," << end - start << ",TRUE\n";
+		caseCount--;
 	}
+	myfile.close();
 	return 0;
 } 
---
+++ selectionsort/cpp/selectionsort.h
@@ -0,0 +1,3 @@
+// the configured options and settings for Tutorial
+#define SELECTIONSORT_VERSION_MAJOR 
+#define SELECTIONSORT_VERSION_MINOR 
